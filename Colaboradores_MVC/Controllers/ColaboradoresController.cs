07882b7720bdec1651166ea60dafc0ccc0a6d4a0
guilhermelanga <guilherme.langa@my.istec.pt>
2021-03-07 15:02:17 +0000

    Colaboradores_MVC

---
+++ Colaboradores_MVC/Controllers/ColaboradoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Colaboradores_MVC.Models;
+
+namespace Colaboradores_MVC.Controllers
+{
+    public class ColaboradoresController : Controller
+    {
+        private EmpresaXEntities1 db = new EmpresaXEntities1();
+
+        // GET: Colaboradores
+        public ActionResult Index()
+        {
+            return View(db.Colaboradores.ToList());
+        }
+
+        // GET: Colaboradores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaboradores colaboradores = db.Colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // GET: Colaboradores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colaboradores/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Colab,Nome,Apelido,Email,BI,NIF,Morada,Concelho,Distrito,Departamento")] Colaboradores colaboradores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Colaboradores.Add(colaboradores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(colaboradores);
+        }
+
+        // GET: Colaboradores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaboradores colaboradores = db.Colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // POST: Colaboradores/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Colab,Nome,Apelido,Email,BI,NIF,Morada,Concelho,Distrito,Departamento")] Colaboradores colaboradores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(colaboradores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(colaboradores);
+        }
+
+        // GET: Colaboradores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Colaboradores colaboradores = db.Colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // POST: Colaboradores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Colaboradores colaboradores = db.Colaboradores.Find(id);
+            db.Colaboradores.Remove(colaboradores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Colaboradores_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Colaboradores_MVC.MvcApplication" Language="C#" %>
