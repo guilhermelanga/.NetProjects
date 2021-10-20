ea75f87901f58108477571b462c885e73606af82
guilhermelanga <guilherme.langa@my.istec.pt>
2021-10-20 14:30:00 +0100

    Seeding data para o model cinema

--- eTickets/eTickets/Data/eTicketsContext.cs
+++ eTickets/eTickets/Data/eTicketsContext.cs
@@ -29,6 +29,15 @@ namespace eTickets.Data
                new Category { Id = 8, Type = "Thriller" }
                 );
 
+            modelBuilder.Entity<Cinema>().HasData(
+                new Cinema { Id =1, Name = "Cinema City Alvalade", Address = "Av. de Roma, nº 100, 1049-15, Lisboa", CinemaLogoURL = "https://www.cinemacity.pt/images/logo.png" },
+                new Cinema { Id =2, Name = "Cinema City Campo Pequeno", Address = "Centro de Lazer, 1000-83, Lisboa", CinemaLogoURL = "https://www.cinemacity.pt/images/logo.png" },
+                new Cinema { Id =3, Name = "Cinema Ideal", Address = "Rua do Loreto, 15/17, 1200-10, Lisboa", CinemaLogoURL = "http://www.cinemaideal.pt/wp-content/uploads/2014/08/logo1.png" },
+                new Cinema { Id =4, Name = "Cinemas Nos Alvaláxia", Address = "Estádio José Alvalade, 1069-7, Lisboa", CinemaLogoURL = "https://cdn.nos.pt/common/img/nos_opengraph.png" },
+                new Cinema { Id =5, Name = "Cinemas Nos Amoreiras", Address = "Av. Eng. Duarte Pacheco, 1070-103, Lisboa", CinemaLogoURL = "https://cdn.nos.pt/common/img/nos_opengraph.png" },
+                new Cinema { Id =6, Name = "UCI Cinemas - El Corte Inglés", Address = "Av. Ant. Aug. Aguiar, 31, 1050-8, Lisboa", CinemaLogoURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVeshPgbj3H8kfSAaY1qhw92ltAhF8nbx_9g&usqp=CAU" }
+                );
+
             base.OnModelCreating(modelBuilder);
         }
     }
